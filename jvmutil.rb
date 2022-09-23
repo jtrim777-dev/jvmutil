32aa6c111ad5bb3873b697a9e92e11ec0ac1fa63
Jake Trimble <jatrimble777@gmail.com>
2022-09-23 15:31:59 -0700

    It makes the symlink

--- jvmutil.rb
+++ jvmutil.rb
@@ -11,23 +11,11 @@ class Jvmutil < Formula
   depends_on "python"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # virtualenv_install_with_resources
     bin.install Dir["bin/*"]
     man1.install Dir["share/man/man1/*"]
     chmod "ugo=rwx", bin/"jvmutil", verbose: true
-  end
 
-  # test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test jvmutil`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    # system "false"
-  # end
+    executable = (bin/"jvmutil").to_s
+    system executable, "relink"
+  end
 end
